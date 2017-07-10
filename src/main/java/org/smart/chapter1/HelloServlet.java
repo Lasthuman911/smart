a427565d20e491feb0bb20d2f8a01a5d1c9f528c
lszhen911 <1078169344@qq.com>
2017-07-10 21:55:23 +0800

    smart

---
+++ src/main/java/org/smart/chapter1/HelloServlet.java
@@ -0,0 +1,35 @@
+package org.smart.chapter1;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+/**
+ * 继承HttpServlet让它成为一个servlet类
+ * Created by lszhen on 2017/7/9.
+ */
+
+@WebServlet("/hello")
+public class HelloServlet extends HttpServlet {
+    /**
+     *
+     * @param req
+     * @param resp
+     * @throws ServletException
+     * @throws IOException
+     */
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+//        super.doGet(req, resp);
+        DateFormat dateFormat= new SimpleDateFormat("yyyy-mm-dd HH:mm:ss");
+        String currentTime = dateFormat.format(new Date());
+        req.setAttribute("currentTime",currentTime);
+        req.getRequestDispatcher("/WEB-INF/jsp/hello.jsp").forward(req,resp);
+    }
+}
